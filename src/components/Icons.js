9ae7395a5b3b3b9c7e5e83589566ff838fa6763a
Surfsol <surfsol@protonamil.com>
2019-12-28 18:12:14 -0300

    remove commented out code Projects.js

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Nav from './Nav'
+import Nav from "./Nav";
 
 import sql from "../assets/icons/sql.png";
 import sass from "../assets/icons/sass.png";
@@ -29,96 +29,104 @@ const Icons = props => {
 
   return (
     <>
-    <div className="navSkills">
-    <Nav/>
-    </div>
-    <div className="icon-wrapper">
-    <div className="techLink">
-      <a href="#HTML" class="tlink">HTML / CSS</a>
-      <a href="#Frontend" class="tlink">Frontend</a>
-      <a href="#Backend" class="tlink">Backend</a>
-      <a href="#Database" class="tlink">Database</a>
+      <div className="navSkills">
+        <Nav />
       </div>
-      <h1>Click to Discover Projects by Technology:</h1>
-      <div className="icons">
-        <div className="icon-group">
-          <h2 >HTML / CSS</h2>
-
-          <NavLink  className="active" to={`/techsProject/html`}>
-            <img src={html5} alt="html" />
-            <h5>HTML </h5>
-          </NavLink>
-
-          <NavLink  className="active" to={`/techsProject/sass`}>
-            <img src={sass} alt="sass" />
-            <h5>SASS</h5>
-          </NavLink>
-
-          <NavLink className="active" to={`/techsProject/less`}>
-            <img src={less} alt="less" />
-            <h5>LESS</h5>
-          </NavLink>
+      <div className="icon-wrapper">
+        <div className="techLink">
+          <a href="#HTML" class="tlink">
+            HTML / CSS
+          </a>
+          <a href="#Frontend" class="tlink">
+            Frontend
+          </a>
+          <a href="#Backend" class="tlink">
+            Backend
+          </a>
+          <a href="#Database" class="tlink">
+            Database
+          </a>
         </div>
+        <h1>Click to Discover Projects by Technology:</h1>
+        <div className="icons">
+          <div className="icon-group">
+            <h2>HTML / CSS</h2>
 
-        <div className="icon-group">
-          <h2 >Frontend</h2>
-          <NavLink className="active" to={`/techsProject/React`}>
-            <img src={react} alt="react" />
-            <h5>React</h5>
-          </NavLink>
+            <NavLink className="active" to={`/techsProject/html`}>
+              <img src={html5} alt="html" />
+              <h5>HTML </h5>
+            </NavLink>
 
-          <NavLink className="active" to={`/techsProject/Redux`}>
-            <img src={redux} alt="redux" />
-            <h5>Redux</h5>
-          </NavLink>
-          <NavLink className="active" to={`/techsProject/js`}>
-            <img src={js} alt="javascript" />
-            <h5>Javascript</h5>
-          </NavLink>
+            <NavLink className="active" to={`/techsProject/sass`}>
+              <img src={sass} alt="sass" />
+              <h5>SASS</h5>
+            </NavLink>
+
+            <NavLink className="active" to={`/techsProject/less`}>
+              <img src={less} alt="less" />
+              <h5>LESS</h5>
+            </NavLink>
+          </div>
+
+          <div className="icon-group">
+            <h2>Frontend</h2>
+            <NavLink className="active" to={`/techsProject/React`}>
+              <img src={react} alt="react" />
+              <h5>React</h5>
+            </NavLink>
+
+            <NavLink className="active" to={`/techsProject/Redux`}>
+              <img src={redux} alt="redux" />
+              <h5>Redux</h5>
+            </NavLink>
+            <NavLink className="active" to={`/techsProject/js`}>
+              <img src={js} alt="javascript" />
+              <h5>Javascript</h5>
+            </NavLink>
+          </div>
+
+          <div className="icon-group">
+            <h2 id="Backend">Backend</h2>
+
+            <NavLink className="active" to={`/techsProject/http`}>
+              <img src={http} alt="http-requests" />
+              <h5>Http-requests</h5>
+            </NavLink>
+
+            <NavLink className="active" to={`/techsProject/18`}>
+              <img src={node} alt="node.js" />
+              <h5> Node.js</h5>
+            </NavLink>
+
+            <NavLink className="active" to={`/techsProject/python`}>
+              <img src={python} alt="python" />
+              <h5>Python</h5>
+            </NavLink>
+
+            <NavLink className="active" to={`/techsProject/django`}>
+              <img src={django} alt="django" />
+              <h5>django</h5>
+            </NavLink>
+
+            <NavLink className="active" to={`/techsProject/Heroku`}>
+              <img src={heroku} alt="heroku" />
+              <h5>Heroku</h5>
+            </NavLink>
+          </div>
+
+          <div className="icon-group">
+            <h2 id="Database">Database</h2>
+            <NavLink className="active" to={`/techsProject/Postgresql`}>
+              <img src={postgresql} alt="postgresql" />
+              <h5>PostgreSQL </h5>
+            </NavLink>
+
+            <NavLink className="active" to={`/techsProject/sql`}>
+              <img src={sql} alt="sql" />
+              <h5>Sql</h5>
+            </NavLink>
+          </div>
         </div>
-
-        <div className="icon-group">
-          <h2 id="Backend">Backend</h2>
-
-          <NavLink className="active" to={`/techsProject/http`}>
-            <img src={http} alt="http-requests" />
-            <h5>Http-requests</h5>
-          </NavLink>
-
-          <NavLink className="active" to={`/techsProject/18`}>
-            <img src={node} alt="node.js" />
-            <h5> Node.js</h5>
-          </NavLink>
-
-          <NavLink className="active" to={`/techsProject/python`}>
-            <img src={python} alt="python" />
-            <h5>Python</h5>
-          </NavLink>
-
-          <NavLink className="active" to={`/techsProject/django`}>
-            <img src={django} alt="django" />
-            <h5>django</h5>
-          </NavLink>
-
-          <NavLink className="active" to={`/techsProject/Heroku`}>
-            <img src={heroku} alt="heroku" />
-            <h5>Heroku</h5>
-          </NavLink>
-        </div>
-
-        <div className="icon-group">
-          <h2 id="Database">Database</h2>
-          <NavLink className="active" to={`/techsProject/Postgresql`}>
-            <img src={postgresql} alt="postgresql" />
-            <h5>PostgreSQL </h5>
-          </NavLink>
-
-          <NavLink className="active" to={`/techsProject/sql`}>
-            <img src={sql} alt="sql" />
-            <h5>Sql</h5>
-          </NavLink>
-        </div>
-      </div>
       </div>
     </>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,243 +11,239 @@ import airbnb from "../assets/images/air2.png";
 import vr from "../assets/images/vr.png";
 import subReddit from "../assets/images/subreddit.png";
 import welldone from "../assets/images/welldone.png";
-import VerticalSpace from "./VerticalSpace";
 
 const Projects = () => {
   return (
     <>
-      <Nav />
-      {/* <VerticalSpace/> */}
-      {/* to apply vertical spacing */}
-      {/* https://medium.com/fed-or-dead/handling-spacing-in-a-ui-component-library-70f3b22ec89 */}
+      <div>
+        <Nav />
+      </div>
 
-      <>
-        <div className="project-wrap">
-          <div className="lambdatitle">
-            <h1>Lambda Team Projects</h1>
+      <div className="project-wrap">
+        <div className="lambdatitle">
+          <h1>Lambda Team Projects</h1>
+        </div>
+
+        <section class="body-content-2">
+          <div class="content">
+            <div class="content-box">
+              <h1>Well Done</h1>
+
+              <p>
+                Our team made additions to the Frontend and Backend
+                architecture. Stakeholder is a NGO that develops sensors for
+                remote well pumps. We converted frontend to Redux, added modals
+                for crud operations, added an Admin and Organizational
+                dashboard, user permissions, etc.
+              </p>
+
+              <ul style={{ listStyle: "none" }}>
+                <h4>Tecnologies applied:</h4>
+                <li>React / Redux</li>
+                <li>React-map</li>
+                <li>Node.js</li>
+                <li>Heroku</li>
+                <li>PostgreSQL</li>
+              </ul>
+              <ul class="link">
+                <a
+                  href="https://github.com/Lambda-School-Labs/well-done-fe"
+                  target="_blank"
+                  class="button"
+                >
+                  Frontend Github Code
+                </a>
+                <br></br>
+                <a
+                  href="https://github.com/Lambda-School-Labs/well-done-be"
+                  target="_blank"
+                  class="button"
+                >
+                  Backend Github Code
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div class="image-body">
+            <img src={welldone} />
+          </div>
+        </section>
+
+        <section className="body-content">
+          <div className="image-body">
+            <img src={subReddit} />
+          </div>
+          <div className="content">
+            <div className="content-box">
+              <h1>SubReddit</h1>
+
+              <p>
+                Design and implement a <strong>BACKEND</strong> server with a
+                database so users can perform
+                <strong> CRUD</strong> operations: Register, Login, .post, .get,
+                .delete, forms, responsive design
+              </p>
+
+              {/* listStyle: "none", no bullet points */}
+              <ul style={{ listStyle: "none" }}>
+                <h4>Tecnologies applied:</h4>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>Knex</li>
+                <li>BcryptJS</li>
+                <li>jsonwebtoken</li>
+                <li>PostgreSql / Sqlite3</li>
+                <li>Heroku</li>
+              </ul>
+              <ul class="link">
+                <a
+                  href="https://github.com/BuildWeek-PostHere-Subreddit"
+                  target="_blank"
+                  class="button"
+                >
+                  Github Code
+                </a>
+              </ul>
+            </div>
+          </div>
+        </section>
+
+        <section class="body-content-2">
+          <div class="content">
+            <div class="content-box">
+              <h1>Yelp</h1>
+
+              <p>
+                Design and implement the Frontend architecture using{" "}
+                <strong>React</strong> and <strong>Redux</strong>. Collaborated
+                with backend and Data Science teams. Register, Login, .post,
+                .get, .put, .delete, forms, responsive design.
+              </p>
+
+              <ul style={{ listStyle: "none" }}>
+                <h4>Tecnologies applied:</h4>
+                <li>React</li>
+                <li>Redux</li>
+                <li>PREPROCESSING</li>
+                <li>Forms</li>
+                <li>ZEIT</li>
+              </ul>
+              <ul class="link">
+                <a
+                  href="https://github.com/BW-YELP-FEELERS"
+                  target="_blank"
+                  class="button"
+                >
+                  Github Code
+                </a>
+              </ul>
+            </div>
           </div>
 
-          <section class="body-content-2">
-            <div class="content">
-              <div class="content-box">
-                <h1>Well Done</h1>
+          <div class="image-body">
+            <img src={yelp} />
+          </div>
+        </section>
 
-                <p>
-                  Our team made additions to the Frontend and Backend
-                  architecture. Stakeholder is a NGO that develops sensors for
-                  remote well pumps. We converted frontend to Redux, added
-                  modals for crud operations, added an Admin and Organizational
-                  dashboard, user permissions, etc.
-                </p>
+        <section class="body-content">
+          <div class="image-body">
+            <img src={airbnb} />
+          </div>
 
-                <ul style={{ listStyle: "none" }}>
-                  <h4>Tecnologies applied:</h4>
-                  <li>React / Redux</li>
-                  <li>React-map</li>
-                  <li>Node.js</li>
-                  <li>Heroku</li>
-                  <li>PostgreSQL</li>
-                </ul>
-                <ul class="link">
-                  <a
-                    href="https://github.com/Lambda-School-Labs/well-done-fe"
-                    target="_blank"
-                    class="button"
-                  >
-                    Frontend Github Code
-                  </a>
-                  <br></br>
-                  <a
-                    href="https://github.com/Lambda-School-Labs/well-done-be"
-                    target="_blank"
-                    class="button"
-                  >
-                    Backend Github Code
-                  </a>
-                </ul>
-              </div>
+          <div class="content">
+            <div class="content-box">
+              <h1>Air Bnb</h1>
+
+              <p>
+                Frontend architecture implementation using{" "}
+                <strong>React</strong>. To perform <strong>CRUD</strong>
+                operations on the database. Register, Login, .post, .get, forms,
+                responsive design.
+              </p>
+
+              <ul style={{ listStyle: "none" }}>
+                <h4>Tecnologies applied:</h4>
+                <li>Javascript</li>
+                <li>React</li>
+                <li>HTML</li>
+                <li>CSS</li>
+              </ul>
+
+              <ul class="link">
+                <a
+                  href="https://github.com/AirBNBOptimalPrice"
+                  target="_blank"
+                  class="button"
+                >
+                  Github Code
+                </a>
+              </ul>
             </div>
-            <div class="image-body">
-              <img src={welldone} />
+          </div>
+        </section>
+
+        <section class="body-content-2">
+          <div class="content">
+            <div class="content-box">
+              <h1>VR Funding</h1>
+
+              <p>
+                Design a fully responsive Landing page for desktop and mobile
+                using preprocessing.
+              </p>
+
+              <ul style={{ listStyle: "none" }}>
+                <h4>Tecnologies applied:</h4>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>PREPROCESSING LESS</li>
+              </ul>
+              <ul class="link">
+                <a
+                  href="https://github.com/Build-Week-VR-Funding-Platform"
+                  target="_blank"
+                  class="button"
+                >
+                  Github Code
+                </a>
+              </ul>
             </div>
-          </section>
+          </div>
 
-          <section className="body-content">
-            <div className="image-body">
-              <img src={subReddit} />
+          <div class="image-body">
+            <img src={vr} />
+          </div>
+        </section>
+
+        <section class="body-content">
+          <div class="image-body">
+            <img src={responsive} />
+          </div>
+
+          <div class="content">
+            <div class="content-box">
+              <h1>Responsive Design</h1>
+              <p>Design a fully responsive static site.</p>
+
+              <ul style={{ listStyle: "none" }}>
+                <h4>Tecnologies applied:</h4>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>LESS</li>
+              </ul>
+              <ul class="link">
+                <a
+                  href="https://github.com/Surfsol/responsive-web-design-I/tree/russell-terry"
+                  target="_blank"
+                  class="button"
+                >
+                  Github Code
+                </a>
+              </ul>
             </div>
-            <div className="content">
-              <div className="content-box">
-                <h1>SubReddit</h1>
-
-                <p>
-                  Design and implement a <strong>BACKEND</strong> server with a
-                  database so users can perform
-                  <strong> CRUD</strong> operations: Register, Login, .post,
-                  .get, .delete, forms, responsive design
-                </p>
-
-                {/* listStyle: "none", no bullet points */}
-                <ul style={{ listStyle: "none" }}>
-                  <h4>Tecnologies applied:</h4>
-                  <li>Node.js</li>
-                  <li>Express</li>
-                  <li>Knex</li>
-                  <li>BcryptJS</li>
-                  <li>jsonwebtoken</li>
-                  <li>PostgreSql / Sqlite3</li>
-                  <li>Heroku</li>
-                </ul>
-                <ul class="link">
-                  <a
-                    href="https://github.com/BuildWeek-PostHere-Subreddit"
-                    target="_blank"
-                    class="button"
-                  >
-                    Github Code
-                  </a>
-                </ul>
-              </div>
-            </div>
-          </section>
-
-          <section class="body-content-2">
-            <div class="content">
-              <div class="content-box">
-                <h1>Yelp</h1>
-
-                <p>
-                  Design and implement the Frontend architecture using{" "}
-                  <strong>React</strong> and <strong>Redux</strong>.
-                  Collaborated with backend and Data Science teams. Register,
-                  Login, .post, .get, .put, .delete, forms, responsive design.
-                </p>
-
-                <ul style={{ listStyle: "none" }}>
-                  <h4>Tecnologies applied:</h4>
-                  <li>React</li>
-                  <li>Redux</li>
-                  <li>PREPROCESSING</li>
-                  <li>Forms</li>
-                  <li>ZEIT</li>
-                </ul>
-                <ul class="link">
-                  <a
-                    href="https://github.com/BW-YELP-FEELERS"
-                    target="_blank"
-                    class="button"
-                  >
-                    Github Code
-                  </a>
-                </ul>
-              </div>
-            </div>
-
-            <div class="image-body">
-              <img src={yelp} />
-            </div>
-          </section>
-
-          <section class="body-content">
-            <div class="image-body">
-              <img src={airbnb} />
-            </div>
-
-            <div class="content">
-              <div class="content-box">
-                <h1>Air Bnb</h1>
-
-                <p>
-                  Frontend architecture implementation using{" "}
-                  <strong>React</strong>. To perform <strong>CRUD</strong>
-                  operations on the database. Register, Login, .post, .get,
-                  forms, responsive design.
-                </p>
-
-                <ul style={{ listStyle: "none" }}>
-                  <h4>Tecnologies applied:</h4>
-                  <li>Javascript</li>
-                  <li>React</li>
-                  <li>HTML</li>
-                  <li>CSS</li>
-                </ul>
-
-                <ul class="link">
-                  <a
-                    href="https://github.com/AirBNBOptimalPrice"
-                    target="_blank"
-                    class="button"
-                  >
-                    Github Code
-                  </a>
-                </ul>
-              </div>
-            </div>
-          </section>
-
-          <section class="body-content-2">
-            <div class="content">
-              <div class="content-box">
-                <h1>VR Funding</h1>
-
-                <p>
-                  Design a fully responsive Landing page for desktop and mobile
-                  using preprocessing.
-                </p>
-
-                <ul style={{ listStyle: "none" }}>
-                  <h4>Tecnologies applied:</h4>
-                  <li>HTML</li>
-                  <li>CSS</li>
-                  <li>PREPROCESSING LESS</li>
-                </ul>
-                <ul class="link">
-                  <a
-                    href="https://github.com/Build-Week-VR-Funding-Platform"
-                    target="_blank"
-                    class="button"
-                  >
-                    Github Code
-                  </a>
-                </ul>
-              </div>
-            </div>
-
-            <div class="image-body">
-              <img src={vr} />
-            </div>
-          </section>
-
-          <section class="body-content">
-            <div class="image-body">
-              <img src={responsive} />
-            </div>
-
-            <div class="content">
-              <div class="content-box">
-                <h1>Responsive Design</h1>
-                <p>Design a fully responsive static site.</p>
-
-                <ul style={{ listStyle: "none" }}>
-                  <h4>Tecnologies applied:</h4>
-                  <li>HTML</li>
-                  <li>CSS</li>
-                  <li>LESS</li>
-                </ul>
-                <ul class="link">
-                  <a
-                    href="https://github.com/Surfsol/responsive-web-design-I/tree/russell-terry"
-                    target="_blank"
-                    class="button"
-                  >
-                    Github Code
-                  </a>
-                </ul>
-              </div>
-            </div>
-          </section>
-        </div>
-      </>
+          </div>
+        </section>
+      </div>
     </>
   );
 };
