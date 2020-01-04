1af412bd1ec48df9a4a0e0804843fc768533a5c8
Surfsol <surfsol@protonamil.com>
2020-01-03 23:30:34 -0300

    Add loginReducer, add loginReducer to index-reducer.js, Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,46 +1,74 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 
-import {fetchLogin} from '../actions/loginAction'
-
+import { fetchLogin } from "../actions/loginAction";
 
 const Login = () => {
-const [creds, setCreds]=useState([])
+  const [creds, setCreds] = useState([]);
 
+  const handleChange = event => {
+    setCreds({ ...creds, [event.target.name]: event.target.value });
+  };
 
-const handleChange = e => {
-    e.preventDefault()
-    
-}
+  const loginR = useSelector(state => state.loginReducer);
 
-return(
+  const dispatch = useDispatch();
+
+  const eventHandler = e => {
+    e.preventDefault();
+    dispatch(fetchLogin(creds));
+  };
+
+  if (loginR.isfetching === true) {
+    return (
+      <>
+        <div className="spin">
+          <div className="spinner"></div>
+          <p>please wait...</p>
+        </div>
+      </>
+    );
+  }
+
+  if (loginR.user.length > 0) {
+    return (
+      <div>
+        <p>Welcome ${loginR.user.username}</p>
+      </div>
+    );
+  }
+
+  return (
     <>
-    <label>
-        Name
-    <input
-    type="text"
-    id="username"
-    placeholder="username"
-    name="username"
-    value={creds.username}
-    onChange={handleChange}
-    />
-    </label>
-
-    <label>
-        Password
-    <input
-    type="text"
-    id="password"
-    placeholder="password"
-    name="password"
-    value={creds.password}
-    onChange={handleChange}
-    />
-    </label>
+      <h2>
+        <label>
+          Name
+          <input
+            type="text"
+            id="username"
+            placeholder="username"
+            name="username"
+            value={creds.username}
+            onChange={handleChange}
+          />
+        </label>
+        </h2>
+        <h2>
+        <label>
+          Password
+          <input
+            type="text"
+            id="password"
+            placeholder="password"
+            name="password"
+            value={creds.password}
+            onChange={handleChange}
+          />
+        </label>
+      </h2>
+      <button onClick={eventHandler}>Submit</button>
     </>
-)
-
-}
-export default Login
+  );
+};
+export default Login;
