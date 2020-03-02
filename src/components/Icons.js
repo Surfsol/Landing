440e37bfa3d6fedb5583de565be8e46a6f29e00a
Surfsol <surfsol@protonamil.com>
2020-03-01 21:31:55 -0300

    adjust icons.scss and nav.scss for mobile

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -17,10 +17,10 @@ import http from "../assets/icons/http.png";
 import redux from "../assets/icons/reduxlogo.png";
 import less from "../assets/icons/less.png";
 import django from "../assets/icons/django.png";
-import aws from "../assets/icons/aws-icon.png"
-import check from "../assets/icons/check.png"
-import graphql from '../assets/images/graphApollo.png'
-import netlify from '../assets/icons/netlify.png'
+import aws from "../assets/icons/aws-icon.png";
+import check from "../assets/icons/check.png";
+import graphql from "../assets/images/graphApollo.png";
+import netlify from "../assets/icons/netlify.png";
 
 import "../assets/css/icons.scss";
 
@@ -58,108 +58,116 @@ const Icons = props => {
         </div>
         <h1>Click to Discover Projects by Technology:</h1>
         <div className="icons">
-          <div className="icon-group">
+          <div className="icon-name">
             <h2>HTML / CSS</h2>
+            <div className="iconsGroup">
+              <NavLink className="active" to={`/techsProject/HTML`}>
+                <img src={html5} alt="html" />
+                <h5>HTML </h5>
+              </NavLink>
 
-            <NavLink className="active" to={`/techsProject/HTML`}>
-              <img src={html5} alt="html" />
-              <h5>HTML </h5>
-            </NavLink>
+              <NavLink className="active" to={`/techsProject/SASS`}>
+                <img src={sass} alt="sass" />
+                <h5>SASS</h5>
+              </NavLink>
 
-            <NavLink className="active" to={`/techsProject/SASS`}>
-              <img src={sass} alt="sass" />
-              <h5>SASS</h5>
-            </NavLink>
-
-            <NavLink className="active" to={`/techsProject/LESS`}>
-              <img src={less} alt="less" />
-              <h5>LESS</h5>
-            </NavLink>
-          </div>
-
-          <div className="icon-group">
-            <h2>Frontend</h2>
-            <NavLink className="active" to={`/techsProject/React`}>
-              <img src={react} alt="react" />
-              <h5>React</h5>
-            </NavLink>
-
-            <NavLink className="active" to={`/techsProject/Redux`}>
-              <img src={redux} alt="redux" />
-              <h5>Redux</h5>
-            </NavLink>
-            <NavLink className="active" to={`/project/sauti`}>
-              <img src={graphql} alt="graphql apollo" />
-              <h5>GraphQl + Apollo</h5>
-            </NavLink>
-            <NavLink className="active" to={`/techsProject/Javascript`}>
-              <img src={js} alt="javascript" />
-              <h5>Javascript</h5>
-            </NavLink>
-          </div>
-
-          <div className="icon-group">
-            <h2 id="Backend">Backend</h2>
-
-            <NavLink className="active" to={`/techsProject/Http-requests`}>
-              <img src={http} alt="http-requests" />
-              <h5>Http-requests</h5>
-            </NavLink>
-
-            <NavLink className="active" to={`/techsProject/Node.js`}>
-              <img src={node} alt="node.js" />
-              <h5> Node.js</h5>
-            </NavLink>
-
-            <NavLink className="active" to={`/project/sauti`}>
-              <img src={graphql} alt="graphql apollo" />
-              <h5>GraphQl + Apollo</h5>
-            </NavLink>
-
-            <NavLink className="active" to={`/techsProject/Python`}>
-              <img src={python} alt="python" />
-              <h5>Python</h5>
-            </NavLink>
-
-            <NavLink className="active" to={`/techsProject/Django`}>
-              <img src={django} alt="django" />
-              <h5>django</h5>
-            </NavLink>
-
-            <NavLink className="active" to={`/techsProject/Testing`}>
-              <img src={check} alt="testing" />
-              <h5>Testing</h5>
-            </NavLink>
-          </div>
-
-          <div className="icon-group">
-            <h2 id="Database">Database</h2>
-            <NavLink className="active" to={`/techsProject/PostgreSQL`}>
-              <img src={postgresql} alt="postgresql" />
-              <h5>PostgreSQL </h5>
-            </NavLink>
-
-            <NavLink className="active" to={`/techsProject/Sql`}>
-              <img src={sql} alt="sql" />
-              <h5>Sql</h5>
-            </NavLink>
-          </div>
-          <div className="icon-group">
-            <h2 id="Hosting">Hosting</h2>
-            <NavLink className="active" to={`/techsProject/Heroku`}>
-              <img src={heroku} alt="heroku" />
-              <h5>Heroku</h5>
-            </NavLink>
-
-            <NavLink className="active" to={`/techsProject/AWS`}>
-              <img src={aws} alt="aws" />
-              <h5>Aws</h5>
-            </NavLink>
-            <NavLink className="active" to={`/project/sauti`}>
-              <img src={netlify} alt="netlify" />
-              <h5>Netlify</h5>
-            </NavLink>
+              <NavLink className="active" to={`/techsProject/LESS`}>
+                <img src={less} alt="less" />
+                <h5>LESS</h5>
+              </NavLink>
             </div>
+          </div>
+
+          <div className="icon-name">
+            <h2>Frontend</h2>
+            <div className="iconsGroup">
+              <NavLink className="active" to={`/techsProject/React`}>
+                <img src={react} alt="react" />
+                <h5>React</h5>
+              </NavLink>
+
+              <NavLink className="active" to={`/techsProject/Redux`}>
+                <img src={redux} alt="redux" />
+                <h5>Redux</h5>
+              </NavLink>
+              <NavLink className="active" to={`/project/sauti`}>
+                <img src={graphql} alt="graphql apollo" />
+                <h5>GraphQl + Apollo</h5>
+              </NavLink>
+              <NavLink className="active" to={`/techsProject/Javascript`}>
+                <img src={js} alt="javascript" />
+                <h5>Javascript</h5>
+              </NavLink>
+            </div>
+          </div>
+
+          <div className="icon-name">
+            <h2 id="Backend">Backend</h2>
+            <div className="iconsGroup">
+              <NavLink className="active" to={`/techsProject/Http-requests`}>
+                <img src={http} alt="http-requests" />
+                <h5>Http-requests</h5>
+              </NavLink>
+
+              <NavLink className="active" to={`/techsProject/Node.js`}>
+                <img src={node} alt="node.js" />
+                <h5> Node.js</h5>
+              </NavLink>
+
+              <NavLink className="active" to={`/project/sauti`}>
+                <img src={graphql} alt="graphql apollo" />
+                <h5>GraphQl + Apollo</h5>
+              </NavLink>
+
+              <NavLink className="active" to={`/techsProject/Python`}>
+                <img src={python} alt="python" />
+                <h5>Python</h5>
+              </NavLink>
+
+              <NavLink className="active" to={`/techsProject/Django`}>
+                <img src={django} alt="django" />
+                <h5>django</h5>
+              </NavLink>
+
+              <NavLink className="active" to={`/techsProject/Testing`}>
+                <img src={check} alt="testing" />
+                <h5>Testing</h5>
+              </NavLink>
+            </div>
+          </div>
+
+          <div className="icon-name">
+            <h2 id="Database">Database</h2>
+            <div className="iconsGroup">
+              <NavLink className="active" to={`/techsProject/PostgreSQL`}>
+                <img src={postgresql} alt="postgresql" />
+                <h5>PostgreSQL </h5>
+              </NavLink>
+
+              <NavLink className="active" to={`/techsProject/Sql`}>
+                <img src={sql} alt="sql" />
+                <h5>Sql</h5>
+              </NavLink>
+            </div>
+          </div>
+          <div className="icon-name">
+            <h2 id="Hosting">Hosting</h2>
+            <div className="iconsGroup">
+              <NavLink className="active" to={`/techsProject/Heroku`}>
+                <img src={heroku} alt="heroku" />
+                <h5>Heroku</h5>
+              </NavLink>
+
+              <NavLink className="active" to={`/techsProject/AWS`}>
+                <img src={aws} alt="aws" />
+                <h5>Aws</h5>
+              </NavLink>
+              <NavLink className="active" to={`/project/sauti`}>
+                <img src={netlify} alt="netlify" />
+                <h5>Netlify</h5>
+              </NavLink>
+            </div>
+          </div>
         </div>
       </div>
     </>
